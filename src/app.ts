3478da28ad988c14619beea359d348571482ef7e
GGTuran <tayyabturan67@gmail.com>
2024-05-16 15:31:50 +0600

    just finished the project set up which doesn't seem fun

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+// const express = require('express')
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+const app: Application = express();
+// const port = 3000
+
+//Parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+});
+
+export default app;
