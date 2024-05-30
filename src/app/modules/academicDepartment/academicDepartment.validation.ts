0d192aa84ea382a92cfd9d0f85484f6031d9c9f9
GGTuran <tayyabturan67@gmail.com>
2024-05-30 22:21:51 +0600

    starting to reference

--- src/app/modules/academicDepartment/academicDepartment.validation.ts
+++ src/app/modules/academicDepartment/academicDepartment.validation.ts
@@ -17,11 +17,11 @@ const updateAcademicDepartmentZod = z.object({
         name:z.string({
             invalid_type_error:'Academic faculty must be a string',
             required_error:'Name is required',
-        }),
+        }).optional(),
         academicFaculty:z.string({
             invalid_type_error:'Academic faculty must be string',
             required_error:'Faculty is required',
-        }),
+        }).optional(),
     }),
 });
 
